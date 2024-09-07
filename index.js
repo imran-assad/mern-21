830911d1235f3fc737dd5eddd613075d362fd057
Imran Asad <imranasad5252@gmail.com>
2024-09-07 19:23:59 +0500

    request received

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ let port = 3000;
 app.listen(port, () => {
   console.log(`Port is listening ${port}`);
 });
+
+app.use((req, res) => {
+  console.log("request received");
+  res.send({
+    name: "Imran Asad",
+    color: "Red",
+  });
+});
