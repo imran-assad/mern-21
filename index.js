60ab35ddc7b9c61b34928b1231d3c2c5ebb67dd5
Imran Asad <imranasad5252@gmail.com>
2024-09-08 22:12:43 +0500

    successfully get data from server

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const app = express();
 
-let port = 3000;
-
-app.listen(port, () => {
-  console.log(`Port is listening ${port}`);
+app.listen(3001, () => {
+  console.log("Listening on port 3000");
 });
 
-app.get("/", (req, res) => {
-  console.log("This is home section");
-});
-app.get("/apple", (res, req) => {
-  console.log("this is apple");
-});
-app.get("/mango", (res, req) => {
-  console.log("this is mango section");
-});
-
-app.post("/banana", (res, req) => {
-  console.log("this is banana section");
+app.get("/register", (req, res) => {
+  let { name, password } = req.query;
+  res.send(`Welcome to the login page ${name}`);
 });
