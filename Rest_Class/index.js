5ac51523d9090e211b6efc3ae11883731161140b
Imran Asad <imranasad5252@gmail.com>
2024-11-23 18:55:46 +0500

    new post added

--- Rest_Class/index.js
+++ Rest_Class/index.js
@@ -10,16 +10,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
+    id: "1a",
     username: "apna college",
     content: "i love coding",
   },
   {
+    id: "2a",
     username: "shraddha",
     content: "Hard work is the key to success",
   },
   {
+    id: "3a",
     username: "Imran",
-    content: "I am still berozgar",
+    content: "I am still learning",
   },
 ];
 
@@ -27,8 +30,20 @@ app.get("/posts", (req, res) => {
   res.render("index", { posts });
 });
 
-app.get("/posts/new", (req, res) => {
-  res.render();
+app.get("/posts/news", (req, res) => {
+  res.render("news.ejs");
+});
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  console.log(req.body);
+  res.redirect("/posts");
+});
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("show.ejs");
 });
 app.listen(port, () => {
   console.log("connection build successfully");
